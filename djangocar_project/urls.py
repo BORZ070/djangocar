6629c36348469d1e6d31663ca8b08a9ecef101ee
BORZ070 <shdgit07@gmail.com>
2023-07-25 20:24:25 +0300

    Update urls.py

--- djangocar_project/urls.py
+++ djangocar_project/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('mainpage_car.urls', namespace='mainpage_car'))
 ]
 
+
 if settings.DEBUG is True:
     urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
