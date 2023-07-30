b7c1e0d24ec63280bdbe8fc70b6017e66fe3910a
BORZ070 <shdgit07@gmail.com>
2023-07-30 15:59:38 +0300

    Update urls.py

--- djangocar_project/urls.py
+++ djangocar_project/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 from mainpage_car.views import main_views, detail_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
