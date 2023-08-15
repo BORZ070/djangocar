6741c83f4278907cb683dc4b560eed712dd21b25
BORZ070 <shdgit07@gmail.com>
2023-08-15 10:50:30 +0300

    old design

--- djangocar_project/urls.py
+++ djangocar_project/urls.py
@@ -7,10 +7,10 @@ from mainpage_car.views import main_views, detail_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_views),
-    path('car/<int:pk>', detail_views)
-    #path('', include('mainpage_car.urls', namespace='mainpage_car'))
+    # path('', main_views),
+    path('car/<int:pk>', detail_views),
+    path('', include('mainpage_car.urls', namespace='mainpage_car'))
 ]
 
 if settings.DEBUG is True:
-    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- temp.py
+++
@@ -1,2 +0,0 @@
-def views(request, pk)
-    return(request):
