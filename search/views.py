0e448cf29a20d3991c6187e6820a445ded23b9a1
BORZ070 <shdgit07@gmail.com>
2023-08-23 11:57:32 +0300

    Update views.py

--- search/views.py
+++ search/views.py
@@ -4,6 +4,7 @@ from mainpage_car.models import Car
 
 def search_views(request):
     query = request.GET.get('text')
+    # print(query)
     cars = Car.objects.all()
     # cars = Car.objects.filter(brand__icontains='BMW')
 
