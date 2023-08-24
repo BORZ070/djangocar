bb12f55614ea2608f16412e63f02aa2d7df90840
BORZ070 <shdgit07@gmail.com>
2023-08-24 11:08:01 +0300

    search 2

--- search/views.py
+++ search/views.py
@@ -4,9 +4,9 @@ from mainpage_car.models import Car
 
 def search_views(request):
     query = request.GET.get('text')
-    # print(query)
-    cars = Car.objects.all()
-    # cars = Car.objects.filter(brand__icontains='BMW')
+
+    # cars = Car.objects.all()
+    cars = Car.objects.filter(brand__icontains=query)
 
     return render(request, 'search.html', {'cars': cars})
 
