58610a14947cefbd7ac3fd4c4784d2fa3924ddd5
BORZ070 <shdgit07@gmail.com>
2023-08-26 18:33:53 +0300

    update 5

--- search/urls.py
+++ search/urls.py
@@ -5,4 +5,5 @@ app_name = 'search'
 
 urlpatterns = [
     path('', search_views, name='search'),
+    path('body/<int:id>', search_views, name='body')
 ]
--- search/views.py
+++ search/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from mainpage_car.models import Car
 
 
-def search_views(request):
-    query = request.GET.get('text')
+def search_views(request, body_id=0):
 
-    # cars = Car.objects.all()
-    cars = Car.objects.filter(brand__icontains=query)
+    query = request.GET.get('text')
+    if query:
+        cars = Car.objects.filter(brand__icontains=query)
+    else:
+        cars = Car.objects.all()
+
+    if body_id !=0:
+        cars = cars.filter(body=body_id)
 
     return render(request, 'search.html', {'cars': cars})
 
