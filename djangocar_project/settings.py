e0f7377e287ce0f830e98fbc2a7cb49053bfa52e
BORZ070 <shdgit07@gmail.com>
2023-09-20 21:44:39 +0300

    update 17

--- djangocar_project/settings.py
+++ djangocar_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'easy_thumbnails',
     'mainpage_car',
     'about',
     'search',
